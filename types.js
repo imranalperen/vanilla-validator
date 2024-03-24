c14e4cd2c72eddafd4db7619a68b2a1170b446d5
imranalperen <imranalperenbayram@gmail.com>
2024-03-24 04:18:32 +0300

    extend added

---
+++ types.js
@@ -0,0 +1,11 @@
+class Types {
+  static OBJECT = "object";
+  static UNDEFINED = "undefined";
+  static STRING = "string";
+  static NUMBER = "number";
+  static SUB_SCHEMA = "sub-schem";
+  static ENUM = "enum";
+  static EXTEND = "extend";
+}
+
+module.exports = { Types };
--- validator.js
+++ validator.js
@@ -1,8 +1,10 @@
+const { Types } = require("./types");
+
 class Validator {
   static validateDTO(data, schema) {
     const returnData = {};
 
-    if (typeof data !== "object" || typeof schema !== "object") {
+    if (typeof data !== Types.OBJECT || typeof schema !== Types.OBJECT) {
       throw new Error("Data and schema must be objects.");
     }
 
@@ -10,7 +12,7 @@ class Validator {
       const expectedType = schema[key].type;
       const required = schema[key].required || true;
       let defaultValue;
-      if (schema[key].default !== "undefined") {
+      if (schema[key].default !== Types.UNDEFINED) {
         defaultValue = schema[key].default;
       }
 
@@ -23,7 +25,7 @@ class Validator {
       }
 
       //? string and number handler
-      if (expectedType === "string" || expectedType === "number") {
+      if (expectedType === Types.STRING || expectedType === Types.NUMBER) {
         returnData[key] = data[key];
         const actualType = typeof data[key];
         if (actualType !== expectedType) {
@@ -33,7 +35,7 @@ class Validator {
         }
       }
       //? enum handler
-      else if (expectedType === "enum") {
+      else if (expectedType === Types.ENUM) {
         returnData[key] = data[key];
         const expectedEnums = schema[key].enum;
         const actualData = data[key];
@@ -46,12 +48,16 @@ class Validator {
         }
       }
       //? sub-schema handler
-      else if (expectedType === "sub-schema") {
+      else if (expectedType === Types.SUB_SCHEMA) {
         const subData = Validator.validateDTO(
           data[key].subSchema,
           schema[key].subSchema,
         );
         returnData[key] = { ...subData };
+      }
+      //? extend
+      else if (expectedType === Types.EXTEND) {
+        // console.log('extendable')
       }
       //? undefined handler
       else {
@@ -67,6 +73,18 @@ class Validator {
 
     return returnData;
   }
+
+  //? before use extendDto validate extendable.
+  //? bc extendDto method doesn't validate extendable
+  static extendDto = (extendable, extend, extension) => {
+    if (extendable[extend].type !== Types.EXTEND) {
+      throw new Error(
+        "To extend schema tehere must be (schema.payload?.subSchema?.payload) object",
+      );
+    }
+    Validator.validateDTO(extension);
+    extendable[extend] = extension;
+  };
 }
 
 module.exports = { Validator };
